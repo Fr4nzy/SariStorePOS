71ff41d6bbb6d3f56c808bc5b8e0dad4431c00f4
kimmyneah <kimnatividad621@gmail.com>
2024-01-05 13:37:24 +0800

    [SSP-21][UI]Store Selector Page

--- app/src/main/java/com/projectfkklp/saristorepos/enums/UserStatus.java
+++ app/src/main/java/com/projectfkklp/saristorepos/enums/UserStatus.java
@@ -1,15 +1,53 @@
 package com.projectfkklp.saristorepos.enums;
 
 public enum UserStatus {
-    ACTIVE(1, "Active"),
-    REQUESTED(2, "Requested"),
-    INVITED(3, "Invited");
+    ACTIVE(
+        1,
+        "Active",
+        "Enter",
+        "Leave",
+        "Leaving %s",
+        "Are you sure you want to leave this store?"
+    ),
+    INVITED(
+            2,
+            "Invited",
+            "Accept",
+            "Decline",
+            "Declining Invitation from %s",
+            "Are you sure you want to to decline the invitivation?"
+    ),
+    REQUESTED(
+            3,
+            "Requested",
+            null,
+            "Cancel",
+            "Canceling Request to Join %s",
+            "Are you sure you want to cancel your request?"
+    );
 
     public final int value;
     public final String label;
 
-    UserStatus(int value, String label) {
+    public final String positiveAction;
+
+    public final String negativeAction;
+    public final String confirmationTitle;
+    public final String confirmationMessage;
+
+    UserStatus(
+        int value,
+        String label,
+        String positiveAction,
+        String negativeAction,
+        String confirmationTitle,
+        String confirmationMessage
+    ) {
         this.value = value;
         this.label = label;
+        this.positiveAction = positiveAction;
+        this.negativeAction = negativeAction;
+        this.confirmationTitle=confirmationTitle;
+        this.confirmationMessage=confirmationMessage;
     }
 }
--- app/src/main/java/com/projectfkklp/saristorepos/models/_User.java
+++
@@ -1,9 +0,0 @@
-package com.projectfkklp.saristorepos.models;
-
-import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.Objects;
-
-public class _User {
-
-}
