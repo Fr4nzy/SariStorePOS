e3e5aa1be1f1ae2a9be4f50ec34381fe6a5e48fd
kimmyneah <kimnatividad621@gmail.com>
2024-01-25 14:57:07 +0800

    [SSP-57][Backend] Created DailyTransactionsManager.addTransaction

--- app/src/main/java/com/projectfkklp/saristorepos/managers/DailyTransactionsManager.java
+++ app/src/main/java/com/projectfkklp/saristorepos/managers/DailyTransactionsManager.java
@@ -6,15 +6,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.projectfkklp.saristorepos.models.DailyTransactions;
+import com.projectfkklp.saristorepos.models.Product;
+import com.projectfkklp.saristorepos.models.Store;
 import com.projectfkklp.saristorepos.models.Transaction;
 import com.projectfkklp.saristorepos.models.TransactionItem;
 import com.projectfkklp.saristorepos.repositories.DailyTransactionsRepository;
 import com.projectfkklp.saristorepos.repositories.SessionRepository;
+import com.projectfkklp.saristorepos.repositories.StoreRepository;
+import com.projectfkklp.saristorepos.utils.DateUtils;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public class DailyTransactionsManager {
     public static CollectionReference getCollectionReference(Context context) {
@@ -28,8 +32,37 @@ public class DailyTransactionsManager {
         DocumentReference dailyTransactionsDocument = DailyTransactionsRepository
             .getDocument(context, date);
 
-        return dailyTransactionsDocument
-            .get()
+        return StoreRepository
+            .getStoreById(SessionRepository.getCurrentStore(context).getId())
+            // Update Store Details: daily sales & products stocks
+            .continueWith(task->{
+                Store store = task.getResult().toObject(Store.class);
+                assert store != null;
+
+                // Update Daily Sales
+                updateDailySales(store, transaction.calculateTotalSales());
+
+                // Update Product Stocks
+                for (TransactionItem transactionItem:transaction.getItems()){
+                    Optional<Product> optionalProduct = store
+                        .getProducts()
+                        .stream()
+                        .filter(p->p.getId().equals(transactionItem.getProductId()))
+                        .findFirst();
+
+                    if (!optionalProduct.isPresent()){
+                        throw new Exception("Product Not Found");
+                    }
+
+                    Product product = optionalProduct.get();
+
+                    product.setStocks(product.getStocks()-transactionItem.getQuantity());
+                }
+
+                return StoreManager.save(store);
+            })
+            // Add transaction to todayDailyTransactions
+            .continueWithTask(task-> dailyTransactionsDocument.get())
             .continueWith(task->{
                 DailyTransactions todayDailyTransactions = task.getResult().toObject(DailyTransactions.class);
 
@@ -42,9 +75,40 @@ public class DailyTransactionsManager {
 
                 return dailyTransactionsDocument.set(todayDailyTransactions);
             })
-            .addOnFailureListener(task->{
-                task.printStackTrace();
-            })
         ;
+    }
+
+    private static void updateDailySales(Store store, double newTransactionAmount){
+        List<Double> dailySales = store.getDailySales();
+        Date lastUpdatedAt = store.getDailySalesUpdatedAt();
+        Date currentDate = new Date();
+
+        store.setDailySalesUpdatedAt(currentDate);
+
+        //If no daily sales, adds a new transaction amount
+        if (dailySales.size()==0) {
+            dailySales.add(newTransactionAmount);
+            return;
+        }
+
+        // If there is existing entry for today
+        if(DateUtils.isSameDay(lastUpdatedAt, currentDate)){
+            int lastItemIndex = dailySales.size()-1;
+            Double updatedDailySale = dailySales.get(lastItemIndex) + newTransactionAmount;
+            dailySales.set(dailySales.size()-1, updatedDailySale);
+            return;
+        }
+
+        // updatedAt and currentDate is not equal
+        {
+            // get gaps for difference of two or more, and fill it with zeros
+            long gaps = DateUtils.calculateDaysDifference(lastUpdatedAt, currentDate) - 1;
+            for (int i=0; i<gaps;i++){
+                dailySales.add(0.);
+            }
+
+            // Append the newTransactionAmount at the end of List
+            dailySales.add(newTransactionAmount);
+        }
     }
 }
