8c9f2047947aa34539205772127f337af52410ec
kimmyneah <kimnatividad621@gmail.com>
2024-01-27 15:14:31 +0800

    [SSP-29] Revisions

--- app/src/main/java/com/projectfkklp/saristorepos/activities/inventory/InventoryProductDetailPage.java
+++ app/src/main/java/com/projectfkklp/saristorepos/activities/inventory/InventoryProductDetailPage.java
@@ -1,6 +1,5 @@
 package com.projectfkklp.saristorepos.activities.inventory;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
