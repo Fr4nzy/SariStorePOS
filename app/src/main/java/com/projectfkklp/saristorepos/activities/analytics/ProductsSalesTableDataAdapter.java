b302961668bfdd3c4c413b03800ab6c5463d475b
kimmyneah <kimnatividad621@gmail.com>
2024-01-30 13:00:48 +0800

    Added SortableTable in Analytics Page

---
+++ app/src/main/java/com/projectfkklp/saristorepos/activities/analytics/ProductsSalesTableDataAdapter.java
@@ -0,0 +1,39 @@
+package com.projectfkklp.saristorepos.activities.analytics;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.projectfkklp.saristorepos.classes.ProductSalesSummaryData;
+import com.projectfkklp.saristorepos.utils.StringUtils;
+
+import java.util.List;
+
+import de.codecrafters.tableview.TableDataAdapter;
+
+public class ProductsSalesTableDataAdapter extends TableDataAdapter<ProductSalesSummaryData> {
+
+    public ProductsSalesTableDataAdapter(Context context, List<ProductSalesSummaryData> data) {
+        super(context, data);
+    }
+    @Override
+    public View getCellView(int rowIndex, int columnIndex, ViewGroup parentView) {
+        ProductSalesSummaryData summary = getRowData(rowIndex);
+        TextView renderedView = new TextView(getContext());
+
+        switch (columnIndex) {
+            case 0:
+                renderedView.setText(summary.productName);
+                break;
+            case 1:
+                renderedView.setText(String.valueOf(summary.soldItems));
+                break;
+            case 2:
+                renderedView.setText(StringUtils.formatToPeso(summary.sales));
+                break;
+        }
+
+        return renderedView;
+    }
+}
