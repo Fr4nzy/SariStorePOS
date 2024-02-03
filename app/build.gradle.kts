a6cd4576903bfaf4826321d9214a56d0bc072f98
Franco Perez <franzy.czolo22@gmail.com>
2024-02-03 22:37:24 +0800

    Updated google-services.json with the new sha1 from Google Play Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,12 @@ android {
 
 dependencies {
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
-    implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
 
