9091cb8d2a8f9aded17b355c9b726709870a5cc3
Franco Perez <franzy.czolo22@gmail.com>
2024-02-06 11:24:26 +0800

    Version 7 - Minor Bug Fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.projectfkklp.saristorepos"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "6"
+        versionCode = 7
+        versionName = "7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
